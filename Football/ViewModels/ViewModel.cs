49df28881dd69c943f73c73e56bc02b9a3896f62
Itay Fridman <fitay123@gmail.com>
2019-01-06 18:16:08 +0200

    ViewModel and gitignore

---
+++ Football/ViewModels/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Football.Models;
+
+namespace Football.ViewModels
+{
+    public class ViewModel
+    {
+        public List<Player> players { get; set; }
+        public List<Staff> staffs { get; set; }
+    }
+}
