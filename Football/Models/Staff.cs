ecafd331a3e203e1a766364bf318e8a271410c87
Itay Fridman <fitay123@gmail.com>
2019-01-06 17:55:05 +0200

    Adding Data Anotations

--- Football/Models/Staff.cs
+++ Football/Models/Staff.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,9 +8,14 @@ namespace Football.Models
 {
     public class Staff
     {
+        [Required]
         public string firstName { get; set; }
+        [Required]
         public string lastName { get; set; }
+        [Required]
+        [Key]
         public string job { get; set; }
+        [Required]
         public int age { get; set; }
 
     }
