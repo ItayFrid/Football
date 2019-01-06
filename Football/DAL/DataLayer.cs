7701415244f2c405a707d2bd1544d49a566e216f
Idan <idanah24@gmail.com>
2019-01-06 18:03:44 +0200

    DL

---
+++ Football/DAL/DataLayer.cs
@@ -0,0 +1,20 @@
+﻿using Football.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Football.DAL
+{
+    public class DataLayer : DbContext
+    {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Player>().ToTable("Players");
+        }
+
+        public DbSet<Player> players { get; set; }
+    }
+}
