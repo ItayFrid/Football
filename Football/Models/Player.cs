95b7efb432939ba2a0ea539558f617e27350c4b5
Itay Fridman <fitay123@gmail.com>
2019-01-07 14:26:28 +0200

    Data Annotations for player and staff

--- Football/Models/Player.cs
+++ Football/Models/Player.cs
@@ -9,8 +9,10 @@ namespace Football.Models
     public class Player
     {
         [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Length must be between 2-50")]
         public string firstName { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Length must be between 2-50")]
         public string lastName { get; set; }
         [Required]
         [RegularExpression("^(100|[1-9][0-9]?)$", ErrorMessage = "Must be a number between 1-100")]
@@ -20,6 +22,7 @@ namespace Football.Models
         [RegularExpression("^([1-9][0-9]?)$", ErrorMessage = "Must be a number between 1-99")]
         public string number { get; set; }
         [Required]
+        [StringLength(3, MinimumLength = 2, ErrorMessage = "Length must be between 2-3")]
         public string position { get; set; }
 
     }
--- Football/Models/Staff.cs
+++ Football/Models/Staff.cs
@@ -9,13 +9,17 @@ namespace Football.Models
     public class Staff
     {
         [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Length must be between 2-50")]
         public string firstName { get; set; }
         [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Length must be between 2-50")]
         public string lastName { get; set; }
-        [Required]
         [Key]
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Length must be between 2-50")]
         public string job { get; set; }
         [Required]
+        [RegularExpression("^([2-7][0-9])$", ErrorMessage = "Must be a number between 20-79")]
         public string age { get; set; }
 
     }
